c53b76ebe604bd698f59c4644c10fa1ba46a0a7a
Vũ Văn Học <2vhoc7@gmail.com>
2025-01-25 23:02:17 +0700

    Create lib.py

---
+++ lib.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
+     
